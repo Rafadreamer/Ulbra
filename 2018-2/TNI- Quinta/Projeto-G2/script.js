b69f5be574b946d95425cac3939b736e0ea00c3a
rafael <rafaelcruzdacosta@gmail.com>
2018-11-29 04:34:29 -0200

    g2----99,99

--- 2018-2/TNI- Quinta/Projeto-G2/script.js
+++ 2018-2/TNI- Quinta/Projeto-G2/script.js
@@ -7,13 +7,18 @@
 
 // rolagem suave .,.,.,.,
 
-    JQuery(document).ready( function($){
-   $(".scroll").click(function(event){
-       Event.preventDefault();
-       $('html,body').animate({scrollTop:$(this.hash).offset().top},700) 
-   })
-
-});
+$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  })
 
 // formulario,,.,.,.,.,.,.,
 
